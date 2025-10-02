36a00a95ac8d3c03f4ad84fd45fbd43456a0ffd0
Matthias Tausch <m.tausch89@gmail.com>
2025-10-02 11:47:28 +0200

    first add

---
+++ scripts/db-meals.js
@@ -0,0 +1,41 @@
+let myPizzas = [
+    {
+        "name": "Pizza Krabben",
+        "price": 9.50,
+        "description": "mit Krabben und Peperoni"
+    }, {
+        "name": "pizza Margherita",
+        "price": 5.90,
+        "description": "mit Mozzarella"
+    },
+    {
+        "name": "Pizza Diavolo (Scharf)",
+        "price": 8.50,
+        "description": "mit Salami, Zwiebeln, Paperoni und Knoblauch"
+    },
+    {
+        "name": "Pizzabrötchen",
+        "price": 12.90,
+        "description": "mit Käse und Knoblauch"
+    }
+]
+
+let mySalads = [
+    {
+        "name": "Kleiner Salat",
+        "price": 6.90,
+        "description": "mit Eisbergsalat, Tomaten, Gurke und Mais"
+    }, {
+        "name": "Großer Salat",
+        "price": 9.90,
+        "description": "mit Eisbersalat, TOmaten, Gurke, Mais, Peperoni und Feldsalat"
+    }, {
+        "name": "Ceasars Salad",
+        "price": 12.90,
+        "description": "Römersalat, Croûtons und einem cremigen Dressing mit Ei, Sardellen, Parmesan und Knoblauch"
+    }, {
+        "name": "Coleslaw Salat",
+        "price": 8.00,
+        "description": "Krautsalat, Karotten, Zwiebeln, Mayonnaise, Buttermilch, Zitronensaft und Zucker"
+    }
+]
