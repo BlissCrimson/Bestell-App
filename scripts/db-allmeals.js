be94214f55a8c700f1efcf2dc8fc9208460129ed
Matthias Tausch <m.tausch89@gmail.com>
2025-10-14 18:53:36 +0200

    first add

---
+++ scripts/db-allmeals.js
@@ -0,0 +1,99 @@
+let allMeals = [
+    {
+        "name": "Pizza Krabben",
+        "price": 9.50,
+        "description": "mit Krabben und Peperoni",
+        "category": "pizza"
+    }, {
+        "name": "Pizza Margherita",
+        "price": 5.90,
+        "description": "mit Mozzarella",
+        "category": "pizza"
+    }, {
+        "name": "Pizza Diavolo (Scharf)",
+        "price": 8.50,
+        "description": "mit Salami, Zwiebeln, Paperoni und Knoblauch",
+        "category": "pizza"
+    },
+    {
+        "name": "Pizza Pollo",
+        "price": 8.50,
+        "description": "mit Hähnchenbrust, Mais, Broccoli und Souce Hollandaise",
+        "category": "pizza"
+    }, {
+        "name": "Pizza Salami",
+        "price": 8.50,
+        "description": "mit Salami",
+        "category": "pizza"
+    }, {
+        "name": "Pizza Funghi",
+        "price": 8.50,
+        "description": "mit Pilzen, Schinken, und Zwiebeln",
+        "category": "pizza"
+    }, {
+        "name": "Pizzabrötchen",
+        "price": 12.90,
+        "description": "mit Käse und Knoblauch",
+        "category": "pizza"
+    }, {
+        "name": "Kleiner Salat",
+        "price": 6.90,
+        "description": "mit Eisbergsalat, Tomaten, Gurke und Mais",
+        "category": "salad"
+    }, {
+        "name": "Großer Salat",
+        "price": 9.90,
+        "description": "mit Eisbersalat, TOmaten, Gurke, Mais, Peperoni und Feldsalat",
+        "category": "salad"
+    }, {
+        "name": "Ceasars Salad",
+        "price": 12.90,
+        "description": "Römersalat, Croûtons und einem cremigen Dressing mit Ei, Sardellen, Parmesan und Knoblauch",
+        "category": "salad"
+    }, {
+        "name": "Coleslaw Salat",
+        "price": 8.00,
+        "description": "Krautsalat, Karotten, Zwiebeln, Mayonnaise, Buttermilch, Zitronensaft und Zucker",
+        "category": "salad"
+    }, {
+        "name": "Coca Cola 1L",
+        "price": 6.90,
+        "description": "Erfrischendes Cola-Getränk, eiskalt serviert",
+        "category": "drinks"
+    }, {
+        "name": "Coca Cola Zero 1L",
+        "price": 6.90,
+        "description": "Zuckerfreier Cola-Geschmack – eiskalt serviert",
+        "category": "drinks"
+    }, {
+        "name": "Fanta Orange 1L",
+        "price": 6.50,
+        "description": "Erfrischendes Orangengetränk mit Kohlensäure",
+        "category": "drinks"
+    }, {
+        "name": "Sprite 1L",
+        "price": 6.50,
+        "description": "Zitronenlimonade mit prickelnder Frische",
+        "category": "drinks"
+    }, {
+        "name": "Mezzo Mix 1L",
+        "price": 6.70,
+        "description": "Kombination aus Cola und Orange – klassisch erfrischend",
+        "category": "drinks"
+    }, {
+        "name": "Mineralwasser Still 0.75L",
+        "price": 4.90,
+        "description": "Natürliches Mineralwasser ohne Kohlensäure",
+        "category": "drinks"
+    }, {
+        "name": "Mineralwasser Spritzig 0.75L",
+        "price": 4.90,
+        "description": "Erfrischendes Mineralwasser mit Kohlensäure",
+        "category": "drinks"
+    }, {
+        "name": "Red Bull 0.25L",
+        "price": 4.50,
+        "description": "Energy Drink – belebt Körper und Geist",
+        "category": "drinks"
+    }
+]
