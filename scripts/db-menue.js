bd1c3811aa2397a1e9b745d110d8236ac5bfeb6c
Matthias Tausch <m.tausch89@gmail.com>
2025-10-28 22:19:09 +0100

    first add

---
+++ scripts/db-menue.js
@@ -0,0 +1,65 @@
+const myPizzas = [
+    {
+        "menu": {
+            "main_disches": [
+                {
+                    "name": "Pizza Krabben",
+                    "price": 9.50,
+                    "description": "mit Krabben und Peperoni"
+                },
+                {
+                    "name": "Pizza Margherita",
+                    "price": 5.90,
+                    "description": "mit Mozzarella"
+                },
+                {
+                    "name": "Pizza Diavolo (Scharf)",
+                    "price": 8.50,
+                    "description": "mit Salami, Zwiebeln, Paperoni und Knoblauch"
+                },
+                {
+                    "name": "Pizza Pollo",
+                    "price": 8.50,
+                    "description": "mit Hähnchenbrust, Mais, Broccoli und Souce Hollandaise"
+                },
+                {
+                    "name": "Pizza Salami",
+                    "price": 8.50,
+                    "description": "mit Salami"
+                },
+                {
+                    "name": "Pizza Funghi",
+                    "price": 8.50,
+                    "description": "mit Pilzen, Schinken, und Zwiebeln"
+                },
+                {
+                    "name": "Pizzabrötchen",
+                    "price": 12.90,
+                    "description": "mit Käse und Knoblauch"
+                }
+            ],
+            "salad_dishes": [
+                {
+                    "name": "Kleiner Salat",
+                    "price": 6.90,
+                    "description": "mit Eisbergsalat, Tomaten, Gurke und Mais"
+                },
+                {
+                    "name": "Großer Salat",
+                    "price": 9.90,
+                    "description": "mit Eisbersalat, TOmaten, Gurke, Mais, Peperoni und Feldsalat"
+                },
+                {
+                    "name": "Ceasars Salad",
+                    "price": 12.90,
+                    "description": "Römersalat, Croûtons und einem cremigen Dressing mit Ei, Sardellen, Parmesan und Knoblauch"
+                },
+                {
+                    "name": "Coleslaw Salat",
+                    "price": 8.00,
+                    "description": "Krautsalat, Karotten, Zwiebeln, Mayonnaise, Buttermilch, Zitronensaft und Zucker"
+                },
+            ]
+        }
+    }
+]
