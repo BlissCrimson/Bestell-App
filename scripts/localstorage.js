659e30b13c8801fc5eff04182c5ae5866a324d3c
Matthias Tausch <m.tausch89@gmail.com>
2025-10-28 16:31:30 +0100

    feat: add meal title, price and amount to local Storage

--- scripts/localstorage.js
+++ scripts/localstorage.js
@@ -1,18 +1,28 @@
 function saveToLocalStorage() {
-    localStorage.setItem("basket", JSON.stringify(basket));
+    localStorage.setItem("mealTitle", JSON.stringify(basket.mealTitle));
+    localStorage.setItem("price", JSON.stringify(basket.price));
+    localStorage.setItem("amount", JSON.stringify(basket.amount));
 }
 
 function getFromLocalStorage() {
-    let mealTitle = JSON.parse(localStorage.getItem(basket.mealTitle));
-    let mealPrice = JSON.parse(localStorage.getItem(basket.price));
-    let mealAmount = JSON.parse(localStorage.getItem(basket.amount));
-    if (basket.mealTitle != null) {
-        basket.mealTitle = JSON.parse(mealTitle);
-        basket.price = JSON.parse(mealPrice);
-        basket.amount = JSON.parse(mealAmount);
+    let mealTitleLS = localStorage.getItem("mealTitle");
+    let priceLS = localStorage.getItem("price");
+    let amountLS = localStorage.getItem("amount");
+    if (mealTitleLS != null) {
+        basket.mealTitle = JSON.parse(mealTitleLS);
+        basket.price = JSON.parse(priceLS);
+        basket.amount = JSON.parse(amountLS);        
     } else {
-        basket.mealTitle = []
-        basket.price = []
-        basket.amount = []
+        basket.mealTitle = [];
+        basket.price = [];
+        basket.amount = [];
     }
+    // let mealTitle = JSON.parse(localStorage.getItem(basket.mealTitle));
+    // let mealPrice = JSON.parse(localStorage.getItem(basket.price));
+    // let mealAmount = JSON.parse(localStorage.getItem(basket.amount));
+    // if (mealTitle) {
+    //     basket.mealTitle = JSON.parse(mealTitle);
+    //     basket.price = JSON.parse(mealPrice);
+    //     basket.amount = JSON.parse(mealAmount);
+    // }
 }
